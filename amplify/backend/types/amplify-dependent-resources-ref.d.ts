57aed653b4be8e1b09885f9400f917cbe0e27b96
Abraham Dier Spiez <abraham7regar@student.gunadarma.ac.id>
2022-02-06 01:22:46 +0700

    Adding Some Updates

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,8 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "spotify": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
